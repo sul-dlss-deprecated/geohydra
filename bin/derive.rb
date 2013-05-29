d63f1c2e27dfe95bced61448e4c83fe4ee0ecb7f
Darren Hardy <drh@stanford.edu>
2013-05-29 16:47:13 -0700

    force clean WKT

--- bin/derive.rb
+++ bin/derive.rb
@@ -2,6 +2,9 @@
 require 'fileutils'
 WORKDIR = '/var/geomdtk/current/workspace'
 
+# ogr2ogr is using a different WKT than GeoServer -- this one is from GeoServer 2.3
+WKT = %q{GEOGCS["WGS 84", DATUM["World Geodetic System 1984", SPHEROID["WGS 84", 6378137.0, 298.257223563,AUTHORITY["EPSG","7030"]], AUTHORITY["EPSG","6326"]], PRIMEM["Greenwich", 0.0, AUTHORITY["EPSG","8901"]], UNIT["degree",0.017453292519943295], AXIS["Geodetic longitude", EAST], AXIS["Geodetic latitude", NORTH], AUTHORITY["EPSG","4326"]]}
+
 def do_system cmd
   puts cmd
   system(cmd)
@@ -13,7 +16,10 @@ def main(workdir = WORKDIR)
     dstfn = File.join(File.dirname(fn), 'EPSG', '4326', File.basename(fn))
     puts "mkdir -p #{File.dirname(dstfn)}"
     FileUtils.mkdir_p File.dirname(dstfn)
-    do_system("ogr2ogr -progress -t_srs EPSG:4326 '#{dstfn}' '#{fn}'") unless File.exist? dstfn
+    unless File.exist? dstfn
+      do_system("ogr2ogr -progress -t_srs EPSG:4326 '#{dstfn}' '#{fn}'") 
+      File.open(dstfn.gsub(%r{shp$}, 'prj'), 'w').write(WKT)
+    end
   end
 end
 
