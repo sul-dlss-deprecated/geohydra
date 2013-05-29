828e5b48a8abb17370d8c798230215c3e2fde888
Darren Hardy <drh@stanford.edu>
2013-05-29 09:47:47 -0700

    Added more unit tests

--- spec/geonetwork_spec.rb
+++ spec/geonetwork_spec.rb
@@ -15,14 +15,14 @@ describe GeoMDTK::GeoNetwork do
   
   before(:each) do
     @client = GeoMDTK::GeoNetwork.new
+    @uuid_example = "FA6ED959-7DED-4722-B1FB-A85FB79725BA"
   end
 
-  describe "#fetch_by_uuid" do
-
+  describe "#fetch" do
     it "verify identificationInfo" do
       %w{FA6ED959-7DED-4722-B1FB-A85FB79725BA}.each do |uuid|
         begin
-          doc = @client.fetch_by_uuid(uuid).content
+          doc = @client.fetch(uuid).content
           fileId = doc.xpath('/gmd:MD_Metadata/gmd:fileIdentifier/gco:CharacterString/text()')
           fileId.to_s.should == uuid
           title = doc.xpath("/gmd:MD_Metadata/" + 
@@ -30,7 +30,7 @@ describe GeoMDTK::GeoNetwork do
                     "gmd:citation/gmd:CI_Citation/" + 
                     "gmd:title/gco:CharacterString").text
           title.should == "Carbon Dioxide (CO2) Pipelines in the United States, 2011"
-        rescue RestGeoNetwork::Forbidden => e
+        rescue RestClient::Forbidden => e
           ap e
         end
         
@@ -38,8 +38,12 @@ describe GeoMDTK::GeoNetwork do
     end
     
     it "info" do
-      r = @client.info(['site', 'users', 'groups', 'sources', 'operations'])
-      r.keys.size.should == 5
+      ['site', 'users', 'groups', 'sources', 'operations'].each do |k|
+        r = @client.info([k])
+        r.keys.size.should == 1
+        doc = Nokogiri::XML(r[k].to_s)
+        doc.xpath("/info/#{k}").size.should > 0
+      end
     end
     
     it "info groups" do
@@ -56,9 +60,10 @@ describe GeoMDTK::GeoNetwork do
       expect { @client.info(%w{site groups nonsense}) }.to raise_error(ArgumentError)
     end
     
-    it "search #each_uuid" do
+    it "search #each" do
       @client.each do |uuid|
-        ap uuid
+        uuid.size.should == @uuid_example.size
+        %r{^[-\da-fA-F]+$}.match(uuid.upcase).should_not == nil
       end
     end
   end
