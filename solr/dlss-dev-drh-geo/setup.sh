995f852c0ea45ea509645830dbe8331b2aa463b7
Darren Hardy <drhardy@github.com>
2013-05-31 15:38:08 -0700

    relocate solr

--- solr/dlss-dev-drh-geo/setup.sh
+++
@@ -1,3 +0,0 @@
-find -L tmp -name '*_iso19139.xml' | while read fn; do
-	xsltproc lib/gmd2solr.xslt $fn > "data/$(basename $fn _iso19139.xml | tr A-Z a-z).xml"
-done
--- solr/push.sh
+++
@@ -1,3 +0,0 @@
-for d in dlss-dev-drh-geo; do
-  rsync -avL $d/ dlss-dev-drh:$d/
-done
