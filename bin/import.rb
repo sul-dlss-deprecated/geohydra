e09e62c29be127876e56d6d3a2c3a74e066b27aa
Darren Hardy <drh@stanford.edu>
2013-06-06 11:57:44 -0700

    Added FC conversion to extra XML file

--- bin/import.rb
+++ bin/import.rb
@@ -3,6 +3,7 @@ require 'geomdtk'
 
 ARGV.each do |fn|
   ofn = File.join('tmp', File.basename(fn).gsub(%r{\.shp\.xml$}, '_geoMetadata.xml'))
-  GeoMDTK::Transform.from_arcgis fn, ofn
+  ofn_fc = File.join('tmp', File.basename(fn).gsub(%r{\.shp\.xml$}, '_FeatureCatalog.xml'))
+  GeoMDTK::Transform.from_arcgis fn, ofn, ofn_fc
 end
 
--- lib/geomdtk/transform.rb
+++ lib/geomdtk/transform.rb
@@ -1,27 +1,40 @@
-require 'tempfile'
-
 module GeoMDTK
+  # Facilitates XSLT stylesheet transformations for ISO 19139 import/export
   module Transform
+    # XSLT file locations
     XSLT = {
       :mods => File.join(File.dirname(__FILE__), 'iso19139_to_mods.xsl'),
       :arcgis => '/var/lib/tomcat6/webapps/geonetwork/xsl/conversion/import/ArcGIS2ISO19139.xsl', # pre-installed ~1MB
       :arcgis_fc => File.join(File.dirname(__FILE__), 'arcgis_to_iso19139_fc.xsl')
     }
     
-    def self.to_mods geoMetadata
-      IO::popen(['xsltproc', XSLT[:mods], '-'].join(' '), 'w+') do |f|
-        f << geoMetadata
+    # XSLT processor
+    XSLTPROC = 'xsltproc --novalid --xinclude'
+    # XML cleaner
+    XMLLINT = 'xmllint --format --xinclude --nsclean'
+    
+    # Converts a ISO 19139 into MODS v3
+    # @param [String] geoMetadata Input data as ISO 19139 XML.
+    # @param [Boolean] validate - if true, uses Nokogiri::XML to parse the geoMetadata before transforming
+    # @return [Nokogiri::XML::Document] the MODS v3 metadata
+    def self.to_mods geoMetadata, validate = false
+      IO::popen("#{XSLTPROC} #{XSLT[:mods]} - | #{XMLLINT} -", 'w+') do |f|
+        f << validate ? geoMetadata : Nokogiri::XML(geoMetadata).to_xml
         f.close_write
         return Nokogiri::XML(f.read)
       end
     end
     
     # Converts an ESRI ArcCatalog metadata.xml into ISO 19139
-    def self.from_arcgis fn, ofn
-      system("xsltproc --novalid --xinclude #{XSLT[:arcgis]} '#{fn}' | xmllint --xinclude --format -o '#{ofn}' -")
-      ofn = File.join(File.dirname(ofn), File.basename(ofn, '.xml') + '_FC.xml')
-      system("xsltproc --novalid --xinclude #{XSLT[:arcgis_fc]} '#{fn}' | xmllint --xinclude --format -o '#{ofn}' -")
+    # @param [String] fn Input file
+    # @param [String] ofn Output file
+    # @param [String] ofn_fc Output file for the Feature Catalog (optional)
+    def self.from_arcgis fn, ofn, ofn_fc = nil
+      system("#{XSLTPROC} #{XSLT[:arcgis]} '#{fn}' | #{XMLLINT} -o '#{ofn}' -")
+      unless ofn_fc.nil?
+        system("#{XSLTPROC} #{XSLT[:arcgis]} '#{fn}' | #{XMLLINT} -o '#{ofn_fc}' -")
+      end
     end
-
+    
   end
 end
