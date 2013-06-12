e5b623e0c73125ca0653a447a2c81e1cf71a7e86
Darren Hardy <drh@stanford.edu>
2013-06-12 09:48:35 -0700

    working

--- solr/dlss-dev-drh-geo/test.sh
+++ solr/dlss-dev-drh-geo/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
-c=dlss-dev-drh-geo-201306_shard1_replica3
-curl -X POST -H 'Content-Type: text/xml' \
-	--data-binary @example_doc.xml \
-	"http://localhost:8080/solr/${c}/update?commit=true"
+c=dlss-dev-drh-geo  # collection
+h=localhost:8983    # jetty
+# h=localhost:8080  # tomcat
+for f in example*.xml; do
+  curl  -X POST \
+        -H 'Content-Type: text/xml' \
+	      --data-binary @$f \
+	      "http://${h}/solr/${c}/update?commit=true"
+done
