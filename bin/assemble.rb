b4ba5a5c7627d1c34f0800b5d6a309eb911f1941
Darren Hardy <drh@stanford.edu>
2013-06-18 13:10:17 -0700

    Added support for thumbnails in MEF

--- bin/assemble.rb
+++ bin/assemble.rb
@@ -7,96 +7,111 @@ def do_system cmd
   if cmd.is_a? Array
     cmd = cmd.join(' ')
   end
-  puts "RUNNING: #{cmd}"
+  puts "RUNNING: #{cmd}" if $DEBUG
   system(cmd.to_s)
+end
+
+def doit client, uuid, obj, flags
+  # setup
+  druid = DruidTools::Druid.new(obj.druid, flags[:workspacedir])
+  raise ArgumentError unless DruidTools::Druid.valid?(druid.druid)
+  [druid.path, druid.content_dir, druid.metadata_dir, druid.temp_dir].each do |d|
+    unless File.directory? d
+      $stderr.puts "Creating directory #{d}" if flags[:verbose]
+      FileUtils.mkdir_p d 
+    end
+  end
+  
+  # export MEF -- the .iso19139.xml file is preferred
+  puts "Exporting MEF for #{uuid}" if flags[:verbose]
+  client.export(uuid, flags[:tmpdir])
+  do_system(['unzip', '-oq', 
+             "#{flags[:tmpdir]}/#{uuid}.mef", 
+              "-d", "#{flags[:tmpdir]}"])
+
+  found_metadata = false
+  %w{metadata.iso19139.xml metadata.xml}.each do |fn| # priority order
+    unless found_metadata
+      fn = File.join(flags[:tmpdir], uuid, 'metadata', fn)
+      next unless File.exist? fn
+
+      found_metadata = true
+
+      # original ISO 19139
+      ifn = File.join(druid.temp_dir, 'iso19139.xml')
+      FileUtils.install fn, ifn, :verbose => flags[:verbose]
+      File.delete fn
+
+      # GeoMetadataDS
+      gfn = File.join(druid.metadata_dir, 'geoMetadata.xml')
+      puts "Generating #{gfn}" if flags[:verbose] 
+      File.open(gfn, "w") do |f|
+        f << GeoMDTK::Transform.to_geoMetadataDS(ifn)
+      end
+
+      # MODS from GeoMetadataDS
+      dfn = File.join(druid.metadata_dir, 'descMetadata.xml')
+      puts "Generating #{dfn}" if flags[:verbose]   
+      File.open(dfn, "w") do |f|
+        f << GeoMDTK::Transform.to_mods(gfn)
+      end
+
+      # Solr document from GeoMetadataDS
+      sfn = File.join(druid.temp_dir, 'solr.xml')
+      puts "Generating #{sfn}" if flags[:verbose]
+      h = GeoMDTK::Transform.to_solr(gfn)
+      doc = Nokogiri::XML::Builder.new do |xml|
+        xml.add {
+          xml.doc_ {
+            h.keys.each do |k|
+              h[k].each do |v|
+                xml.field v, :name => k
+              end
+            end
+          }
+        }
+      end
+      File.open(sfn, "w") { |f| f << doc.to_xml }
+    end
+  end
+
+  raise ArgumentError, "Cannot extract MEF metadata: #{uuid}: Missing metadata.xml" unless found_metadata
+  
+  # export any thumbnail images
+  ['png', 'jpg'].each do |fmt|
+    Dir.glob(File.join(flags[:tmpdir], uuid, '{private,public}', ('*.' + fmt))) do |fn|
+      ext = '.' + fmt
+      tfn = File.basename(fn, ext)
+      tfn += 'mall' if tfn =~ %r{_s$} # convert _s to _small as per GeoNetwork convention
+      FileUtils.install fn, File.join(druid.content_dir, tfn + ext), :verbose => flags[:verbose]
+    end
+  end
+
+  # export content into zip files
+  Dir.glob(File.join(flags[:stagedir], "#{druid.id}.zip")) do |fn|
+    # extract shapefile name using filename pattern from
+    # http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
+    k = %r{([a-zA-Z0-9_-]+)\.(shp|tif)$}i.match(`unzip -l #{fn}`)[1] 
+    ofn = "#{druid.content_dir}/#{k}.zip"
+    FileUtils.install fn, ofn, :verbose => flags[:verbose]
+  end
 end
 
 def main flags
   client = GeoMDTK::GeoNetwork.new
   client.each do |uuid|
-    puts "Processing #{uuid}"
-    obj = client.fetch(uuid)
-    unless obj.druid
-      # raise ArgumentError, "uuid #{uuid} missing druid"
-      $stderr.puts "WARNING: uuid #{uuid} is missing Druid"
-      next
+    begin
+      puts "Processing #{uuid}"
+      obj = client.fetch(uuid)
+      # unless obj.druid
+      #   # raise ArgumentError, "uuid #{uuid} missing druid"
+      #   $stderr.puts "WARNING: uuid #{uuid} is missing Druid"
+      #   next
+      # end
+      doit client, uuid, obj, flags
+    rescue Exception => e
+      $stderr.puts e
     end
-    
-    # setup
-    druid = DruidTools::Druid.new(obj.druid, flags[:workspacedir])
-    raise ArgumentError unless DruidTools::Druid.valid?(druid.druid)
-    [druid.path, druid.content_dir, druid.metadata_dir].each do |d|
-      unless File.directory? d
-        ap "Creating directory #{d}"
-        FileUtils.mkdir_p d 
-      end
-    end
-
-    # export MEF -- the .iso19139.xml file is preferred
-    puts "Exporting MEF for #{uuid}"
-    client.export(uuid, flags[:tmpdir])
-    do_system(['unzip', '-jo', 
-               "#{flags[:tmpdir]}/#{uuid}.mef", 
-               "#{uuid}/metadata/metadata*.xml",
-                "-d", flags[:tmpdir]])
-    found_metadata = false
-    %w{metadata.iso19139.xml metadata.xml}.each do |fn| # priority order
-      unless found_metadata
-        fn = File.join(flags[:tmpdir], fn)
-        next unless File.exist? fn
-      
-        found_metadata = true
-        
-        # original ISO 19139
-        ifn = File.join(druid.temp_dir, 'iso19139.xml')
-        puts "Copying #{fn} => #{ifn}"
-        FileUtils.install fn, ifn
-        File.delete fn
-
-        # GeoMetadataDS
-        gfn = File.join(druid.metadata_dir, 'geoMetadata.xml')
-        puts "Generating #{gfn}"      
-        File.open(gfn, "w") do |f|
-          f << GeoMDTK::Transform.to_geoMetadataDS(ifn)
-        end
-      
-        # MODS from GeoMetadataDS
-        dfn = File.join(druid.metadata_dir, 'descMetadata.xml')
-        puts "Generating #{dfn}"      
-        File.open(dfn, "w") do |f|
-          f << GeoMDTK::Transform.to_mods(gfn)
-        end
-      
-        # Solr document from GeoMetadataDS
-        sfn = File.join(druid.temp_dir, 'solr.xml')
-        puts "Generating #{sfn}"
-        h = GeoMDTK::Transform.to_solr(gfn)
-        doc = Nokogiri::XML::Builder.new do |xml|
-          xml.add {
-            xml.doc_ {
-              h.keys.each do |k|
-                h[k].each do |v|
-                  xml.field v, :name => k
-                end
-              end
-            }
-          }
-        end
-        File.open(sfn, "w") { |f| f << doc.to_xml }
-      end
-    end
-    
-    raise ArgumentError, "Cannot export MEF metadata: #{uuid}: Missing #{flags[:tmpdir]}/metadata.xml" unless found_metadata
-    
-    # export content into zip files
-    Dir.glob(File.join(flags[:stagedir], "#{druid.id}.zip")) do |fn|
-      # extract shapefile name using filename pattern from
-      # http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
-      k = %r{([a-zA-Z0-9_-]+)\.shp$}i.match(`unzip -l #{fn}`)[1] 
-      ofn = "#{druid.content_dir}/#{k}.zip"
-      puts "Copying GIS data: #{fn} -> #{ofn}"
-      FileUtils.install fn, ofn
-    end    
   end
 end
 
@@ -104,7 +119,7 @@ end
 begin
   File.umask(002)
   flags = {
-    :verbose => true,
+    :verbose => false,
     :stagedir => GeoMDTK::Config.geomdtk.stage || 'stage',
     :workspacedir => GeoMDTK::Config.geomdtk.workspace || 'workspace',
     :tmpdir => GeoMDTK::Config.geomdtk.tmpdir || 'tmp'
@@ -114,8 +129,11 @@ begin
     opts.banner = <<EOM
 Usage: #{File.basename(__FILE__)} [options]
 EOM
-    opts.on("-v", "--[no-]verbose", "Run verbosely (default: #{flags[:verbose]})") do |v|
+    opts.on("-v", "--verbose", "Run verbosely (default: #{flags[:verbose]})") do |v|
       flags[:verbose] = v
+    end
+    opts.on("-q", "--quiet", "Run quietly (default: #{not flags[:verbose]})") do |v|
+      flags[:verbose] = (not v)
     end
     opts.on("--stagedir DIR", "Staging directory with ZIP files (default: #{flags[:stagedir]})") do |v|
       flags[:stagedir] = v
