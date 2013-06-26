155370bc02512dbae6ee2b06ad364789e0dfaa80
Darren Hardy <drh@stanford.edu>
2013-06-25 22:31:10 -0700

    Add all content and stacks connections

--- spec/test_net_ssh_krb.rb
+++ spec/test_net_ssh_krb.rb
@@ -3,10 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 
 Dor::DigitalStacksService # force Net::SSH load
 
-h = Dor::Config.stacks.host
-u = Dor::Config.stacks.user
-a = 'gssapi-with-mic'
-puts "Connecting to #{u}@#{h} via #{a}"
-Net::SSH.start(h, u, :auth_methods => [a]) do |ssh|
-  puts ssh.exec!("/bin/echo test completed ok")
+def doit h, u, p = '', a = 'gssapi-with-mic'
+  puts "Connecting to #{u}@#{h} via #{a}"
+  Net::SSH.start(h, u, :auth_methods => [a]) do |ssh|
+    puts ssh.exec!("/bin/ls -aC #{p}")
+  end
 end
+
+doit  Dor::Config.stacks.host, 
+      Dor::Config.stacks.user,
+      Dor::Config.stacks.storage_root
+doit  Dor::Config.stacks.document_cache_host, 
+      Dor::Config.stacks.document_cache_user, 
+      Dor::Config.stacks.document_cache_storage_root
+doit  Dor::Config.content.content_server, 
+      Dor::Config.content.content_user, 
+      Dor::Config.content.content_base_dir
