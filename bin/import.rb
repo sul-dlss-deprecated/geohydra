f9e9f39fff719b72505fd29277cc3e6ac4fc7237
Darren Hardy <drh@stanford.edu>
2013-06-27 16:31:32 -0700

    ArcGIS to ISO tool for preping import to GeoNetwork

--- bin/import.rb
+++ bin/import.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
-require 'geomdtk'
+require File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 
 ARGV.each do |fn|
+  $stderr.puts "Processing #{fn}"
   ofn = File.join('tmp', File.basename(fn).gsub(%r{\.shp\.xml$}, '_geoMetadata.xml'))
   ofn_fc = File.join('tmp', File.basename(fn).gsub(%r{\.shp\.xml$}, '_FeatureCatalog.xml'))
   GeoMDTK::Transform.from_arcgis fn, ofn, ofn_fc
