b9d61efd4a056070b18f971256a6dcd692c4cbc9
Darren Hardy <drh@stanford.edu>
2013-07-03 09:51:09 -0700

    code cleanup

--- bin/accession.rb
+++ bin/accession.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+# encoding: UTF-8
+
 require File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 require 'optparse'
 require 'druid-tools'
@@ -12,7 +14,7 @@ begin
     :admin_policy => 'druid:cb854wz7157',
     :rights => 'stanford',
     :tags => [
-      "Registered With : GeoMDTK",
+      'Registered With : GeoMDTK',
       "Registered By : #{%x{whoami}.strip}"
     ],
     :tmpdir => GeoMDTK::Config.geomdtk.tmpdir || 'tmp',
@@ -24,61 +26,58 @@ begin
     :debug => false,
     :workspacedir => GeoMDTK::Config.geomdtk.workspace || 'workspace'
   }
-  
+
   OptionParser.new do |opts|
     opts.version = VERSION
     opts.banner = <<EOM
 Usage: #{File.basename(__FILE__)} [options] [druid [druid...]]
 EOM
-    opts.on("--apo DRUID", "APO for collection to accession" + (flags[:admin_policy] ? " (default: #{flags[:admin_policy]})" : '')) do |druid|
+    opts.on('--apo DRUID', 'APO for collection to accession' + (flags[:admin_policy] ? " (default: #{flags[:admin_policy]})" : '')) do |druid|
       flags[:admin_policy] = DruidTools::Druid.new(druid).druid
     end
-    opts.on("--collection DRUID", "Collection for accession" + (flags[:collection] ? " (default: #{flags[:collection]})" : '')) do |druid|
+    opts.on('--collection DRUID', 'Collection for accession' + (flags[:collection] ? " (default: #{flags[:collection]})" : '')) do |druid|
       flags[:collection] = DruidTools::Druid.new(druid).druid
     end
-    opts.on("--purge", "Purge items before accessioning") do |b|
+    opts.on('--purge', 'Purge items before accessioning') do |b|
       flags[:purge] = true
     end
-    opts.on("-q", "--quiet", "Run quietly") do
-      flags[:verbose] = false
-    end
-    opts.on("--rights KEYWORD", "Rights keyword (default: #{flags[:rights]})") do |keyword|
+    opts.on('--rights KEYWORD', "Rights keyword (default: #{flags[:rights]})") do |keyword|
       flags[:rights] = keyword
     end
-    opts.on("--tag TAG", "Tag for each object - multiple tags allowed") do |tag|
+    opts.on('--tag TAG', 'Tag for each object - multiple tags allowed') do |tag|
       flags[:tags] << tag
     end
-    opts.on("--test", "Verify configuration then exit") do 
+    opts.on('--test', 'Verify configuration then exit') do
       flags[:configtest] = true
     end
-    opts.on("--tmpdir DIR", "Temporary directory for assembly (default: #{flags[:tmpdir]})") do |d|
+    opts.on('--tmpdir DIR', "Temporary directory for assembly (default: #{flags[:tmpdir]})") do |d|
       flags[:tmpdir] = d
     end
-    opts.on("--upload[=MAX]", "Upload content files -- MAX restricts to files smaller than MAX MB") do |mb|
+    opts.on('--upload[=MAX]', 'Upload content files -- MAX restricts to files smaller than MAX MB') do |mb|
       flags[:upload] = true
       flags[:upload_max] = (mb.to_f < 0 ? Float::INFINITY : mb.to_f) unless mb.nil?
     end
-    opts.on("-v", "--verbose", "Run verbosely, use multiple times for debug level output") do 
+    opts.on('-v', '--verbose', 'Run verbosely, use multiple times for debug level output') do
       flags[:debug] = true if flags[:verbose]  # -vv
       flags[:verbose] = true
     end
-    opts.on("--workspace DIR", "Workspace directory for assembly (default: #{flags[:workspacedir]})") do |d|
+    opts.on('--workspace DIR', "Workspace directory for assembly (default: #{flags[:workspacedir]})") do |d|
       flags[:workspacedir] = d
-    end    
+    end
   end.parse!
-  
+
   [flags[:tmpdir], flags[:workspacedir]].each do |d|
     raise ArgumentError, "Missing directory #{d}" unless File.directory? d
   end
-  
+
   ap({:flags => flags}) if flags[:debug]
-  
+
   # Verify configuation
   if flags[:configtest]
     %w{workflow.url fedora.safeurl solrizer.url ssl.cert_file ssl.key_pass dor.service_root}.each do |k|
       begin
         k = "Dor::Config.#{k}"
-        $stderr.puts "ERROR: Configuration requires #{k}" if eval(k).nil?
+        $stderr.puts "ERROR: Configuration requires #{k}"
       rescue Exception => e
         $stderr.puts e, e.backtrace
       end
