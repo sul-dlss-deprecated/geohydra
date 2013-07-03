880b78309a793c983b74a952410d808f8b9461fd
Darren Hardy <drh@stanford.edu>
2013-07-03 15:48:32 -0700

    Solr loader

---
+++ solr/ogp-dev/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+c=ogp-dev # collection
+h=localhost:8983    # jetty
+# h=localhost:8080  # tomcat
+find -L /var/geomdtk/current/workspace -name 'ogp*.xml' | while read f; do
+  echo "Uploading $f /solr/${c}/update"
+  curl  -X POST \
+        -H 'Content-Type: text/xml' \
+	      --data-binary @$f \
+	      "http://${h}/solr/${c}/update?commit=true"
+done
+
+echo curl "http://${h}/solr/${c}/update?optimize=true"
+
