4cbbe58150c961815d2d49f6d854c553fc09cf28
Darren Hardy <drh@stanford.edu>
2013-07-30 13:04:11 -0700

    cleanup

--- solr/ogp-dev/test.sh
+++ solr/ogp-dev/test.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 c=ogp-dev # collection
-h=ogpapp-dev.stanford.edu    # ogpdev
-#h=localhost:8983    # jetty
-# h=localhost:8080  # tomcat
-find -L /var/geomdtk/current/workspace -name 'ogp*.xml' | while read f; do
-  echo "Uploading $f /solr/${c}/update"
+#h=localhost:8080            # tomcat
+h=ogpapp-dev.stanford.edu  # httpd
+#h=localhost:8983           # jetty
+d=/var/geomdtk/current/workspace
+
+# XXX: include HTTPS authentication
+
+find -L "$d" -name 'ogpSolr.xml' | while read fn; do
+  echo "Uploading $fn"
   curl  -X POST \
         -H 'Content-Type: text/xml' \
-	      --data-binary @$f \
-	      "http://${h}/solr/${c}/update?commit=true"
+        --data-binary @- \
+        "http://${h}/solr/${c}/update" \
+        < "$fn"
 done
 
 curl "http://${h}/solr/${c}/update?commit=true"
