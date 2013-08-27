f5704332908c26238778e2375d0e436728a454a3
Darren Hardy <drh@stanford.edu>
2013-08-27 10:42:19 -0700

    add .json to stage

--- scripts/load_stage.rb
+++ scripts/load_stage.rb
@@ -4,10 +4,18 @@ require 'fileutils'
 require 'druid-tools'
 
 Dir.chdir('/var/geomdtk/current/stage')
+puts 'scanning upload/druid for zips'
 Dir.glob(File.join('/var/geomdtk/current/upload/druid/**/*.zip')) do |zipfn|
   druid = DruidTools::Druid.new(File.basename(File.dirname(File.dirname(zipfn))))
   FileUtils.ln_sf(zipfn, "#{druid.id}.zip", :verbose => true)
-  Dir.glob(File.join(File.dirname(zipfn), '..', '**', '*-iso19139.xml')) do |xmlfn|
-    FileUtils.ln_sf(File.expand_path(xmlfn), "#{druid.id}.xml", :verbose => true)
-  end
+end
+
+Dir.glob(File.join('/var/geomdtk/current/upload/druid/**/temp/*-iso19139.xml')) do |xmlfn|
+  druid = DruidTools::Druid.new(File.basename(File.dirname(File.dirname(xmlfn))))
+  FileUtils.ln_sf(File.expand_path(xmlfn), "#{druid.id}.xml", :verbose => true)
+end
+
+Dir.glob(File.join('/var/geomdtk/current/upload/druid/**/temp/geoOptions.json')) do |optfn|
+  druid = DruidTools::Druid.new(File.basename(File.dirname(File.dirname(optfn))))
+  FileUtils.ln_sf(File.expand_path(optfn), "#{druid.id}.json", :verbose => true)
 end
