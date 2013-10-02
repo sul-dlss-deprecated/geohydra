17fd4dc8a4ee5e1ae8d138400210392ea69cd4ab
Darren Hardy <drh@stanford.edu>
2013-10-02 15:54:35 -0700

    script to load mods into JSON from document cache

---
+++ scripts/ingest_mods.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+mods = {}
+Dir.glob('/Volumes/Geo3TB/document_cache/??/???/??/????/mods') do |fn|
+  puts fn
+  if fn =~ %r{(../\d\d\d/../\d\d\d\d)/mods$}
+    druid = $1.gsub(/\//, '')
+  else 
+    raise ArgumentError, fn
+  end
+  mods[druid] = File.read(fn)
+end
+JSON.dump(mods, File.open('mods.json', 'w'))
