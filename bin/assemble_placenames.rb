60fe080054ad03023b29b2f71e8c7556a62d2564
Darren Hardy <drh@stanford.edu>
2013-11-19 14:49:26 -0800

    Fix geo extension

--- bin/assemble_placenames.rb
+++ bin/assemble_placenames.rb
@@ -7,22 +7,36 @@ require 'json'
 
 @@g = GeoHydra::Gazetteer.new
 
+#
+# Resolves placenames using local gazetteer
+#
+#   * Changes subject/geographic with GeoNames as authority to have the correct valueURI
+#   * Adds correct rdf:resource to geo extension
+#
 def resolve_placenames(modsFn, flags)
+  puts "Processing #{modsFn}" if flags[:verbose]
   mods = Nokogiri::XML(File.open(modsFn, 'rb'))
-  r = mods.xpath('//mods:geographic', { 'mods' => 'http://www.loc.gov/mods/v3' })
+  r = mods.xpath('//mods:geographic[@authority=\'GeoNames\']', { 'mods' => 'http://www.loc.gov/mods/v3' })
   r.each do |i|
     ap({:i => i}) if flags[:debug]
     k = i.content # Gazetteer keyword
     uri = @@g.find_uri_by_keyword(k)
     if uri.nil?
-      puts "WARNING: Missing gazetteer entry for '#{k}'"
+      puts "WARNING: Missing gazetteer entry for '#{k}'" if flags[:verbose]
+      next
     else
       i['valueURI'] = uri
+      coverages = mods.xpath('//dc:coverage', { 'dc' => 'http://purl.org/dc/elements/1.1'})
+      coverages.each do |j|
+      # XXX: need to correct matching dc:coverage@rdf:resource with uri
+        j['rdf:resource'] = uri
+      end
     end
     
     lcnaf = @@g.find_lcnaf_by_keyword(k)
-    ap({:lcnaf => lcnaf})
+    ap({:lcnaf => lcnaf}) if flags[:debug]
     if not (lcnaf.nil? or k == lcnaf)
+      puts "Adding Library of Congress NAF entry to end of MODS record" if flags[:verbose]
       mods.root << Nokogiri::XML("<subject><geographic authorityURI='http://id.loc.gov/authorities/names'>#{lcnaf}</geographic></subject>").root
     end
     ap({:i => i}) if flags[:debug]
@@ -36,7 +50,7 @@ def main(flags)
   puts "Searching for MODS records..." if flags[:verbose]
   puts flags[:workspacedir] + '/**/' + DruidTools::Druid.glob + '/metadata/descMetadata.xml' if flags[:debug]
   Dir.glob(flags[:workspacedir] + '/**/' + DruidTools::Druid.glob + '/metadata/descMetadata.xml') do |modsFn|
-    ap({:modsFn => modsFn})
+    ap({:modsFn => modsFn}) if flags[:debug]
     resolve_placenames(modsFn, flags)
   end
 end
