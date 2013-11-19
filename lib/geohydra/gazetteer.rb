0066a68af9283ac6abcd0060bd505ecc78ef3f93
Darren Hardy <drh@stanford.edu>
2013-11-19 09:59:31 -0800

    Unit tests for gazetteer

--- lib/geohydra/gazetteer.rb
+++ lib/geohydra/gazetteer.rb
@@ -1,13 +1,44 @@
 # encoding: UTF-8
 
+require 'csv'
+require 'awesome_print'
+
 module GeoHydra
   class Gazetteer
     
-    def self.find_by_keyword(kw)
-      nil
+    CSV_FN = File.join(File.dirname(__FILE__), 'gazetteer.csv')
+    
+    def initialize
+      @registry = {}
+      CSV.foreach(CSV_FN, :encoding => 'UTF-8') do |v|#, {:headers => true, }) do |v|
+        @registry[v[0]] = {
+          :id => v[1].to_i,
+          :lcsh => v[2],
+          :uri => "http://geonames.org/#{v[1].to_i}"
+        }
+      end
+    end
+    
+    # @return [Integer] geonames id
+    def find_id_by_keyword(kw)
+      @registry.include?(kw) ? @registry[kw][:id].to_i : nil
+    end
+    
+    # @return [String] library of congress subject heading
+    def find_lcsh_by_keyword(kw)
+      @registry.include?(kw) ? @registry[kw][:lcsh] : nil
+    end
+
+    # @return [String] library of congress subject heading
+    def find_uri_by_keyword(kw)
+      @registry.include?(kw) ? @registry[kw][:uri] : nil
     end
   
-    def self.find_by_id(id)
+    # @return [String] The keyword
+    def find_keyword_by_id(id)
+      @registry.each do |k,v|
+        return k if v[:id] == id
+      end
       nil
     end
   end
--- spec/gazetteer_spec.rb
+++ spec/gazetteer_spec.rb
@@ -8,22 +8,72 @@ require 'awesome_print'
 require 'equivalent-xml'
 require 'geohydra'
 
+g = GeoHydra::Gazetteer.new
+# ap({:g => g})
+
+K2ID = {
+  'United States' => 6252001,
+  'Union Territory of Chandīgarh' => 1274744,
+  'State of Mahārāshtra' => 1264418
+}
+
+K2LCSH = {
+  'United States' => 'United States',
+  'Union Territory of Chandīgarh' => nil,
+  'State of Mahārāshtra' => 'Maharashtra (India)'
+}
+
 describe GeoHydra::Gazetteer do
   
-  describe '#find_by_keyword' do
-    %w{a b c}.each do |k|
+  describe '#find_id_by_keyword' do
+    it "nil case" do
+      g.find_id_by_keyword(nil).should == nil      
+      g.find_id_by_keyword('adsfadsfasdf').should == nil      
+    end
+    K2ID.each do |k,id|
       it k do
-        v = GeoHydra::Gazetteer.find_by_keyword(k)
-        v.should == nil
+        r = g.find_id_by_keyword(k)
+        r.should == id
       end
     end
   end
   
-  describe '#find_by_id' do
-    %w{1 2 3}.each do |k|
+  describe '#find_lcsh_by_keyword' do
+    it "nil case" do
+      g.find_lcsh_by_keyword(nil).should == nil      
+      g.find_lcsh_by_keyword('asdfasdfasdf').should == nil      
+    end
+    K2LCSH.each do |k,lcsh|
       it k do
-        v = GeoHydra::Gazetteer.find_by_id(k.to_i)
-        v.should == nil
+        r = g.find_lcsh_by_keyword(k)
+        r.should == lcsh
+      end
+    end
+  end
+  
+
+  describe '#find_uri_by_keyword' do
+    it "nil case" do
+      g.find_uri_by_keyword(nil).should == nil      
+      g.find_uri_by_keyword('asdfasdfasdf').should == nil      
+    end
+    K2ID.each do |k,id|
+      it k do
+        r = g.find_uri_by_keyword(k)
+        r.should == "http://geonames.org/#{id}"
+      end
+    end
+  end
+  
+  describe '#find_keyword_by_id' do
+    it "nil case" do
+      g.find_keyword_by_id(nil).should == nil      
+      g.find_keyword_by_id(-1).should == nil      
+    end
+    K2ID.each do |k,id|
+      it id do
+        r = g.find_keyword_by_id(id)
+        r.should == k
       end
     end
   end
