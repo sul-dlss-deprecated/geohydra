106cb064567c1a2cec75a30bf6d9e7d5298edbed
Darren Hardy <drh@stanford.edu>
2014-02-05 14:03:17 -0800

    Major cleanup of loader_postgis

--- bin/loader_postgis.rb
+++ bin/loader_postgis.rb
@@ -8,36 +8,9 @@ require 'optparse'
 require 'mods'
 require 'druid-tools'
 require 'dor-services'
-
-require 'active_record'
-require 'active_support'
 require 'tmpdir'
 
-class RegisteredLayer < ActiveRecord::Base
-  attr_accessible :druid, :layername, :title
-  # self.primary_key = 'druid'
-  
-  def find_by_druid(druid)
-    RegisteredLayer.find(:first, :conditions => [ "druid = ?", druid.to_s])
-  end
-end
-
 class LoaderPostGIS < GeoHydra::Process
-
-
-  # ENV['RGEOSERVER_CONFIG'] ||= ENV_FILE + '_rgeoserver.yml'
-  # require 'rgeoserver'
-
-  #=  Input data. *See DATA section at end of file*
-  # The input file is in YAML syntax with each record is a Hash with keys:
-  # - layername
-  # - filename
-  # - format
-  # - title
-  # and optionally
-  # - description
-  # - keywords
-  # - metadata_links
 
   # USAGE: shp2pgsql [<options>] <shapefile> [[<schema>.]<table>]
   # OPTIONS:
@@ -76,138 +49,85 @@ class LoaderPostGIS < GeoHydra::Process
   #       the -I flag is used.
   #   -?  Display this help screen.
 
-  def main conn, layers, flags = {}
-    ap({:layers => layers, :flags => flags}) if flags[:debug]
-    layers.each do |k, v|
-      %w{layername format filename title}.each do |i|
-        raise ArgumentError, "Layer is missing required '#{i}'" if v[i.to_sym].nil? or not v.include?(i.to_sym)
-      end
-      puts "Processing layer #{k}" if flags[:verbose]
-  
-      layername = v[:layername].strip
-      format = v[:format].downcase.strip.to_sym
-      druid = v[:druid]
-      title = v[:title].strip
-      
-      case format
-      when :shapefile      
-        ap({:v => v}) if flags[:debug]
-        Dir.mktmpdir('shp', druid.temp_dir) do |d|
-          begin
-            system("unzip -oj '#{v[:filename]}' -d '#{d}'")
-            system("ls -la #{d}")
-            Dir.glob("#{d}/*.shp") do |shp|
-              # XXX: HARD CODED projection here -- extract from MODS or ISO19139
-              # XXX: Perhaps put the .sql data into the content directory as .zip for derivative
-              # XXX: -G for the geography column causes some issues with GeoServer
-              system("shp2pgsql -s 4326 -d -D -I -W #{flags[:encoding]} '#{shp}' #{flags[:schema]}.#{druid.id} > '#{druid.temp_dir}/#{druid.id}.sql'")
-              system('psql --no-psqlrc --no-password --quiet ' +
-                     "--host='#{flags[:host.to_s]}' " +
-                     "--port='#{flags[:port.to_s]}' " +
-                     "--username='#{flags[:username.to_s]}' " + 
-                     "--dbname='#{flags[:database.to_s]}' " +
-                     "--file='#{druid.temp_dir}/#{druid.id}.sql' ")
-            end
-          rescue Exception => e
-            FileUtils.rm_rf(d) if File.exist?(d)
-          end
+  def main layer, flags, format = :shapefile
+    druid = layer[:druid]
+    zipfn = layer[:zipfn]
+    projection = layer[:projection] || flags[:projection]
+    schema = layer[:schema] || flags[:schema]
+    encoding = layer[:encoding] || flags[:encoding]
+
+    raise NotImplementedError, "Unsupported format: #{format}" unless format == :shapefile
+
+    Dir.mktmpdir('shp', druid.temp_dir) do |d|
+      begin
+        system("unzip -oj '#{zipfn}' -d '#{d}'")
+        system("ls -la #{d}")
+        Dir.glob("#{d}/*.shp") do |shp|
+          # XXX: HARD CODED projection here -- extract from MODS or ISO19139
+          # XXX: Perhaps put the .sql data into the content directory as .zip for derivative
+          # XXX: -G for the geography column causes some issues with GeoServer
+          system("shp2pgsql -s #{projection} -d -D -I -W #{encoding}" +
+                 " '#{shp}' #{schema}.#{druid.id} " +
+                 "> '#{druid.temp_dir}/#{druid.id}.sql'")
+          system('psql --no-psqlrc --no-password --quiet ' +
+                 "--file='#{druid.temp_dir}/#{druid.id}.sql' ")
         end
-      
-        if flags[:register]
-          puts "Registering layer #{druid.id}, #{layername}, #{title}" if flags[:verbose]
-          layer = RegisteredLayer.find_by_druid druid.id
-          ap({:found_layer => layer}) if flags[:debug]
-          if layer.nil?
-            layer = RegisteredLayer.new( 
-              :druid => druid.id,
-              :layername => layername,
-              :title => title
-            )
-          end
-          layer.layername = layername
-          layer.title = title
-          ap({:updated_layer => layer}) if flags[:debug]
-          layer.save
-        end
-      else
-        raise NotImplementedError, "Unsupported format #{format}"    
+      rescue Exception => e
+        FileUtils.rm_rf(d) if File.exist?(d)
       end
     end
   end
 
-  def from_druid druid, flags
+  # locates the data.zip file, preferring one with explicit projection
+  def druid2layer druid, flags
     ap({:druid => druid}) if flags[:debug]
-    prj = flags[:projection] || "EPSG:4326"
-    prj = prj.split(':').join('_')
-    druid = DruidTools::Druid.new(druid, flags[:datadir])
-    mods_fn = druid.path('metadata/descMetadata.xml')
-    puts "Loading #{mods_fn}" if flags[:verbose]
-    mods = Mods::Record.new
-    mods.from_url(mods_fn)
-    ap({:mods => mods}) if flags[:debug]
-
-    geo_fn = druid.path('metadata/geoMetadata.xml')
-    puts "Loading #{geo_fn}" if flags[:verbose]
-    geo = Dor::GeoMetadataDS.from_xml(File.read(geo_fn))
-    ap({:geo => geo}) if flags[:debug]
 
     zipfn = nil
-    layername = nil
     projection = nil
-    Dir.glob(druid.content_dir + "/*_#{prj}.zip") do |fn|
-      puts "Found EPSG 4326 zip: #{fn}" if flags[:verbose]
-      projection = '4326'
-      zipfn = fn
-      layername = File.basename(zipfn, "_#{prj}.zip")
-      puts "Derived layername #{zipfn} -> #{layername}" if flags[:verbose]
-    end
-    if not zipfn
-      Dir.glob(druid.content_dir + "/*.zip") do |fn|
+    %w{4326}.each do |prj|
+      Dir.glob(File.join(druid.content_dir, "data_EPSG_#{prj}.zip")) do |fn|
+        puts "Found EPSG #{prj} zip: #{fn}" if flags[:verbose]
+        projection = prj
         zipfn = fn
-        layername = File.basename(zipfn, '.zip')
       end
     end
-    raise ArgumentError, zipfn unless File.exist?(zipfn) and layername
-    ap({:zipfn => zipfn, :layername => layername}) if flags[:debug]
-    r = { 
-      :vector => {
-        :druid => druid,
-        :format => 'Shapefile',
-        :layername => layername,
-        :filename => zipfn,
-        :title => mods.full_titles.first,
-        :projection => projection.nil?? '4326' : projection
-      }
+    
+    if zipfn.nil?
+      Dir.glob(File.join(druid.content_dir, 'data.zip')) do |fn|
+        puts "Found native zip: #{fn}" if flags[:verbose]
+        zipfn = fn
+        projection = '4326' # XXX: Hardcoded assumption that native work is in WGS84 if missing derived work
+      end
+    end
+
+    raise ArgumentError, "Missing data ZIP file: #{zipfn}" unless File.exist?(zipfn)
+    raise ArgumentError, "Missing Projection" if projection.nil?
+    ap({:zipfn => zipfn}) if flags[:debug]
+
+    { 
+      :druid => druid,
+      :zipfn => zipfn,
+      :projection => projection
     }
-    r
   end
 
   def run(args)
     flags = {
-      :debug => false,
-      :verbose => false,
-      :register => false,
-      :register_drop => false,
-      :register_table => 'registered_layers',
       :encoding => 'UTF-8',
-      :datadir => '/var/geomdtk/current/workspace',
-      :schema => GeoHydra::Config.postgis.schema || 'public'
+      :workspacedir => GeoHydra::Config.geohydra.workspace || 'workspace',
+      :schema => GeoHydra::Config.postgis.schema || 'druid'
     }
   
     OptionParser.new do |opts|
       opts.banner = "
   Usage: #{File.basename(__FILE__)} [-v] [druid ... | < druids]
-  "
-      opts.on("-v", "--verbose", "Run verbosely") do |v|
-        flags[:debug] = true if flags[:verbose]
+"
+      opts.on('-v', '--verbose', 'Run verbosely, use multiple times for debug level output') do
+        flags[:debug] = true if flags[:verbose]  # -vv
         flags[:verbose] = true
       end
-      opts.on("-d DIR", "--datadir DIR", "Data directory (default: #{flags[:datadir]})") do |v|
-        raise ArgumentError, "Invalid directory #{v}" unless File.directory?(v)
-        flags[:datadir] = v
-      end    
-      opts.on("-R", "--register", "Register shapefile") do |v|
-        flags[:register] = true
+      opts.on('--workspace DIR', "Workspace directory for assembly (default: #{flags[:workspacedir]})") do |v|
+        flags[:workspacedir] = v
       end
 
       %w{schema encoding}.each do |k|
@@ -218,44 +138,10 @@ class LoaderPostGIS < GeoHydra::Process
     end.parse!(args)
 
     ap({:flags => flags}) if flags[:debug]
-    dbfn = File.expand_path(File.dirname(__FILE__) + '/../config/database.yml')
-    puts "Loading #{dbfn}" if flags[:verbose]
-    dbconfig = YAML.load(File.read(dbfn))
-    raise ArgumentError, "Missing configuration for environment" unless dbconfig.include?(ENV['GEOHYDRA_ENVIRONMENT'])
-    flags.merge! dbconfig[ENV['GEOHYDRA_ENVIRONMENT']]
-    ap({:flags => flags}) if flags[:debug]
-  
-    ActiveRecord::Base.logger = ActiveSupport::BufferedLogger.new($stderr) if flags[:debug]
-  
-    conn = ActiveRecord::Base.establish_connection flags
-    conn.with_connection do |db|
-      # ap({:obj => db, :klass => db.class, :methods => db.public_methods, :schema_search_path => db.schema_search_path})
-      puts "Connected to PostgreSQL #{db.select_value("SHOW server_version")} " +
-           "using #{db.current_database} database" if flags[:verbose]
-      if db.select_value('select default_version from pg_catalog.pg_available_extensions where name = \'postgis\'') =~ /^(2\.[\.\d]*)$/
-        puts "Using PostGIS #{$1}" if flags[:verbose]
-      else
-        raise NotImplementedError, "Database does not have PostGIS support"
-      end
-      if flags[:register]
-        n = db.select_value("SELECT COUNT(*) FROM pg_tables WHERE schemaname = 'public' and tablename = '#{flags[:register_table]}'")
-        if n.to_i == 0 or flags[:register_drop]
-          puts "Creating registry table in public.#{flags[:register_table]}" if flags[:verbose]
-          db.execute("DROP TABLE public.#{flags[:register_table]}") if flags[:register_drop]
-          db.execute("
-            CREATE TABLE public.#{flags[:register_table]}
-            (
-              druid character varying NOT NULL PRIMARY KEY,
-              layername character varying NOT NULL,
-              title character varying NOT NULL
-            );      
-            ")
-        end
-      end
-    end
-    
+      
     (args.size > 0 ? args : $stdin).each do |s|
-        main(conn, from_druid(s.strip, flags), flags)
+      druid = DruidTools::Druid.new(s.strip, flags[:workspacedir])
+      main(druid2layer(druid, flags), flags)
     end
   end
 end
