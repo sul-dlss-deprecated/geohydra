f9de125e3555e614847ba2455e088664e2550b38
Darren Hardy <drh@stanford.edu>
2014-03-02 20:49:40 -0800

    .

--- solr/kurma-app-dev/create.sh
+++
@@ -1,1 +0,0 @@
-curl 'http://localhost:8080/solr/admin/collections?action=CREATE&name=kurma-app-dev&numShards=1&replicationFactor=3'
--- solr/kurma-app-dev/delete.sh
+++
@@ -1,1 +0,0 @@
-curl 'http://localhost:8080/solr/admin/collections?action=DELETE&name=kurma-app-dev&numShards=1&replicationFactor=3'
