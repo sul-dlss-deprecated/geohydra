3283ed68e83e7ef7b1755912224f13ca04d08603
Darren Hardy <drh@stanford.edu>
2014-03-05 11:47:50 -0800

    sul-solr deploy

---
+++ solr/kurma-app-test/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+export PATH=/usr/bin:/bin
+c=kurma-app-test
+
+if [ "$(hostname)" != 'sul-solr-a.stanford.edu' ]; then
+	echo "Wrong host"
+	exit -1
+fi
+
+cd /home/drh/geohydra/solr/$c/conf || exit -1
+java -cp "/usr/share/tomcat6/webapps/solr/WEB-INF/lib/*:/usr/share/tomcat6/lib/*" \
+  org.apache.solr.cloud.ZkCLI -cmd upconfig -zkhost 127.0.0.1:2181 \
+  -confdir . -confname $c \
+  -solrhome /home/lyberadmin/solr-home
+
